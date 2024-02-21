5eae0248433ebe04533e54a95170b050ffab83f7
jack mattern <jackfm21@yahoo.com>
2024-02-21 11:31:44 -0800

    updated code

--- main.cpp
+++ main.cpp
@@ -10,10 +10,17 @@ int main(int argc, char* argv[]) {
 
   for(int i=1; i < argc; i++) {
     int length = strlen(argv[i]);
+    if (argc < 2) {
+	    cout << "ERR_MISSING: One or more RGB values should be provided as arguments, seperated by space" << endl;
+    }
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
       cout << "#" << argv[i] << endl;
+    }
+    else if (length != 3 || length != 6) {
+      cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
     }  
   }
+  
   return 0;
 }
 
@@ -23,8 +30,8 @@ bool all_hex(char* word) {
       || (word[i] >= 'A' && word[i] <='F')) {
       continue;
     }
-    else {
-      return false;
+    else if (word[i] > 9 || word[i] > 'f' || word[i] > 'F') {
+	    cout << word[i] << " ERR_VALUE: RGB values must be in 0-F range" << endl;
     }
   }
 
