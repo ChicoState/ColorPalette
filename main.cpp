ccf829887158be27422d05376de8e62af0433b9a
bbynocode <dlemm@csuchico.edu>
2024-02-21 11:20:57 -0800

    Made error messages more verbose

--- main.cpp
+++ main.cpp
@@ -12,7 +12,11 @@ int main(int argc, char* argv[]) {
     int length = strlen(argv[i]);
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
       cout << "#" << argv[i] << endl;
-    }  
+    }  else if (all_hex(argv[i]) == false) {
+        cout << argv[i] << " ERR_VALUE: RGB values must be in 0-F range" << endl;
+    }  else if (length != 3 || length != 6) {
+        cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+    } 
   }
   return 0;
 }
