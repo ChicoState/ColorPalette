066202e4a993f1434b1d7296b1e3ce8d1484f9dd
infinimineralex <ivoRobotnik36@gmail.com>
2024-02-21 11:23:05 -0800

    Resolves #1, improves on previous commit

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@ bool all_hex(char*);
 int main(int argc, char* argv[]) {
   int valid_colors = 0;
 
+  if (argc < 3) {
+    cerr << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by space" << endl;
+    return 1;
+  }
+
   for(int i=1; i < argc; i++) {
     int length = strlen(argv[i]);
     if( (length != 3) && (length != 6)) {
-    	cerr << "Your color is of an incorrect length." << endl;
+      cerr << argv[i];
+    	cerr << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
     	return 1;
     }
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
@@ -28,7 +34,8 @@ bool all_hex(char* word) {
       continue;
     }
     else {
-	cerr << "Your color is not all valid hex." << endl;
+      printf(word);
+      cerr << " ERR_VALUE: RGB values must be in 0-F range" << endl;
       return false;
     }
   }
