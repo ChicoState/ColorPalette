bd3a599cbd7b09b1438994b718d878eae0c8dc30
Steven Thao <sthao21@csuchico.edu>
2024-02-21 11:28:43 -0800

    Resolved issue #1

--- main.cpp
+++ main.cpp
@@ -3,31 +3,41 @@
 
 using namespace std;
 
-bool all_hex(char*);
+bool all_hex(char *);
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   int valid_colors = 0;
 
-  for(int i=1; i < argc; i++) {
+  for (int i = 1; i < argc; i++)
+  {
     int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
+    if ((length == 3 || length == 6) && all_hex(argv[i]))
+    {
       cout << "#" << argv[i] << endl;
-    }  
+    }
+    else
+    {
+      cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long " << endl;
+    }
   }
   return 0;
 }
 
-bool all_hex(char* word) {
-  for(int i=0; i<strlen(word); i++) {
-    if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
-      || (word[i] >= 'A' && word[i] <='F')) {
+bool all_hex(char *word)
+{
+  for (int i = 0; i < strlen(word); i++)
+  {
+    if ((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f') || (word[i] >= 'A' && word[i] <= 'F'))
+    {
       continue;
     }
-    else {
+    else
+    {
       return false;
     }
   }
 
-  //no non-hex characters found
+  // no non-hex characters found
   return true;
 }
