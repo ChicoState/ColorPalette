05b3105593e6d9d1af56903c8d571c103fbadb2c
Nathan Whitney <86084524+the29ster@users.noreply.github.com>
2024-02-21 11:25:40 -0800

    added error messages resolves #1

--- main.cpp
+++ main.cpp
@@ -8,11 +8,24 @@ bool all_hex(char*);
 int main(int argc, char* argv[]) {
   int valid_colors = 0;
 
+  if(argc < 2) {
+    cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces" << endl;
+    return -1;
+  }
+
   for(int i=1; i < argc; i++) {
     int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
-    }  
+    if(length == 3 || length == 6) {
+      if(all_hex(argv[i])) {
+        cout << "#" << argv[i] << endl;
+      }
+      else {
+        cout << argv[i] << " ERR_VALUE: RGB values must be in 0-F range" << endl;
+      }
+    }
+    else {
+      cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+    }
   }
   return 0;
 }
