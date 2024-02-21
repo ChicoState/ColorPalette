2e49e6cc1439e0044f824f73b6872610a73913b4
Kailey <kfallbeck13@yahoo.com>
2024-02-21 11:39:54 -0800

    Added error messaging for invalid length and RGB values and insufficient number of arguments, resolves #1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,44 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
 
 bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    cout << "ERR_MISSING: One or more RGB values should be provided as "
+            "arguments, separated by spaces"
+         << endl;
+  }
   int valid_colors = 0;
 
-  for(int i=1; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
+    if (length != 3 && length != 6) {
+      cout << length
+           << " ERR_LENGTH: RGB values must be either 3 or 6 characters long"
+           << endl;
+    }
+    if (all_hex(argv[i])) {
       cout << "#" << argv[i] << endl;
-    }  
+    }
   }
   return 0;
 }
 
 bool all_hex(char* word) {
-  for(int i=0; i<strlen(word); i++) {
-    if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
-      || (word[i] >= 'A' && word[i] <='F')) {
+  for (int i = 0; i < strlen(word); i++) {
+    if ((word[i] >= '0' && word[i] <= '9') ||
+        (word[i] >= 'a' && word[i] <= 'f') ||
+        (word[i] >= 'A' && word[i] <= 'F')) {
       continue;
-    }
-    else {
+    } else {
+      cout << word[i] << " ERR_VALUE: RGB values must be in 0-F range" << endl;
       return false;
     }
   }
 
-  //no non-hex characters found
+  // no non-hex characters found
   return true;
 }
