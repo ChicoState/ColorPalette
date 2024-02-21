3ded0803650195382cb5f94dccd62a8c1096ea8f
Younitea <thomasrchiesa@gmail.com>
2024-02-21 11:13:54 -0800

    Updated error handling to show why specific hex or RGB values ares excluded. In addition it provides an error message when not enough values are supplied

--- main.cpp
+++ main.cpp
@@ -7,20 +7,30 @@ bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
   int valid_colors = 0;
-
+  if(argc <2){
+    cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces\n";
+    return 0;
+  }
   for(int i=1; i < argc; i++) {
     int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
+    bool hex = all_hex(argv[i]);
+    if( (length == 3 || length == 6) && hex ) {
       cout << "#" << argv[i] << endl;
-    }  
-  }
+    }
+    else{
+      if(length != 3 && length !=6) 
+        cout << '#' << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long\n";
+      if(hex)
+        cout << '#' << argv[i] << " ERR_VALUE: RGB values must be in 0-F range\n";
+    }
+  }  
   return 0;
 }
 
 bool all_hex(char* word) {
   for(int i=0; i<strlen(word); i++) {
     if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
-      || (word[i] >= 'A' && word[i] <='F')) {
+        || (word[i] >= 'A' && word[i] <='F')) {
       continue;
     }
     else {
