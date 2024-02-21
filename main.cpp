9e079590e6c91d4143df500fe5618b9aaecc96a3
Blake Hufford <huffordblake@gmail.com>
2024-02-21 11:45:29 -0800

    better error messages resolves #1

--- main.cpp
+++ main.cpp
@@ -10,7 +10,16 @@ int main(int argc, char* argv[]) {
     int length = std::strlen(argv[i]);
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
       std::cout << "#" << argv[i] << std::endl;
-    }  
+    }
+    else if(length != 3 && length != 6){
+      std::cout << argv[i] << "ERR_LENGTH: RGB values must be either 3 or 6 characters long" << std::endl;
+    }
+    else{
+      std::cout << argv[i] << "ERR_VALUE: RGB values must be in 0-F range" << std::endl;
+    }
+  }
+  if(argc < 2){
+    std::cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces" << std::endl;
   }
   return 0;
 }
