3e4b81c13b67692963ef4e7b131e561a1e19793e
yadnyeyavairat <yadnyeyavairat@gmail.com>
2024-02-21 11:32:00 -0800

    Fixed validation logic for RGB colors, #1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
 
-bool all_hex(char*);
+// Function to check if a character is a hexadecimal digit
+bool is_hex_digit(char c) {
+    return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
+}
+
+// Function to check if all characters in a string are hexadecimal digits
+bool all_hex(const char* word) {
+    for(int i = 0; i < strlen(word); i++) {
+        if(!is_hex_digit(word[i])) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(int argc, char* argv[]) {
-  int valid_colors = 0;
+    if (argc < 2) {
+        cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces" << endl;
+        return 1; // Exit with an error code
+    }
 
-  for(int i=1; i < argc; i++) {
-    int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
-    }  
-  }
-  return 0;
+    for(int i = 1; i < argc; i++) {
+        int length = strlen(argv[i]);
+        if(length != 3 && length != 6) {
+            cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+            continue; // Skip further checks and continue with the next iteration
+        }
+        
+        if(!all_hex(argv[i])) {
+            cout << argv[i] << " ERR_VALUE: RGB values must be in 0-F range" << endl;
+            continue; // Skip further checks and continue with the next iteration
+        }
+        
+        // If no errors, print the valid RGB value
+        cout << "#" << argv[i] << endl;
+    }
+    return 0;
 }
 
-bool all_hex(char* word) {
-  for(int i=0; i<strlen(word); i++) {
-    if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
-      || (word[i] >= 'A' && word[i] <='F')) {
-      continue;
-    }
-    else {
-      return false;
-    }
-  }
-
-  //no non-hex characters found
-  return true;
-}
