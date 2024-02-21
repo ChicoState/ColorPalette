27f10dd6274766e9cd900dcdfed0684cb09a6fd2
KVentayen <cammeraprogramming@gmail.com>
2024-02-21 19:26:18 +0000

    Solves #1 for Invalid Character Lengthw

--- main.cpp
+++ main.cpp
@@ -6,28 +6,30 @@ using namespace std;
 bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
-  int valid_colors = 0;
+	int valid_colors = 0;
 
-  for(int i=1; i < argc; i++) {
-    int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
-    }  
-  }
-  return 0;
+	for(int i=1; i < argc; i++) {
+		int length = strlen(argv[i]);
+		if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
+			cout << "#" << argv[i] << endl;
+		} else {
+			cerr << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long\n"; 
+		}
+	}
+	return 0;
 }
 
 bool all_hex(char* word) {
-  for(int i=0; i<strlen(word); i++) {
-    if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
-      || (word[i] >= 'A' && word[i] <='F')) {
-      continue;
-    }
-    else {
-      return false;
-    }
-  }
+	for(int i=0; i<strlen(word); i++) {
+		if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
+				|| (word[i] >= 'A' && word[i] <='F')) {
+			continue;
+		}
+		else {
+			return false;
+		}
+	}
 
-  //no non-hex characters found
-  return true;
+	//no non-hex characters found
+	return true;
 }
