069074d8024e62f12815e53b70327904e1278256
laile823 <lle13@mail.csuchico.edu>
2024-02-21 11:33:18 -0800

    Error checking for length, hexadecimal digit, less than 2 rgb values

--- main.cpp
+++ main.cpp
@@ -8,12 +8,24 @@ bool all_hex(char*);
 int main(int argc, char* argv[]) {
   int valid_colors = 0;
 
-  for(int i=1; i < argc; i++) {
-    int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
-    }  
+  if(argc < 3){
+    cout << "ERR_MISSING: One or more RGB values should be provided as arguments separated by space" << endl;
   }
+    else {
+      for(int i=1; i < argc; i++) {
+        int length = strlen(argv[i]);
+        if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
+          cout << "#" << argv[i] << endl;
+        }
+        if(length != 3 && length != 6 ){
+            cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+          }
+        if(!all_hex(argv[i])){
+          cout << argv[i] << " ERR_VALUE: RGB values must be in 0-f range" << endl;
+        }
+      }
+    }
+
   return 0;
 }
 
