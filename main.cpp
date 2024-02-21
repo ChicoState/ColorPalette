280a880439bb2a2e44745aad62cb8abc6d133455
Solarleaf <124109321+Solarleaf@users.noreply.github.com>
2024-02-21 11:31:51 -0800

    Update to main.cpp
    Properly formats code. Improves error messaging for when input is noncompliant. Input length and type error messages added. resolves #1

--- main.cpp
+++ main.cpp
@@ -1,31 +1,51 @@
+// LH edits 2-21-2024
+
 #include <iostream>
 #include <cstring>
 
-bool all_hex(char*);
+using namespace std;
 
-int main(int argc, char* argv[]) {
+bool all_hex(char *);
+
+int main(int argc, char *argv[])
+{
   int valid_colors = 0;
 
-  for(int i=1; i < argc; i++) {
-    int length = std::strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      std::cout << "#" << argv[i] << std::endl;
-    }  
+  for (int i = 1; i < argc; i++)
+  {
+    int length = strlen(argv[i]);
+    if (length < 3)
+    {
+      cerr << argv[i] << " ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces\n";
+      return 1;
+    }
+    if ((length == 3 || length == 6) && all_hex(argv[i]))
+    {
+      cout << "#" << argv[i] << endl;
+    }
+    else
+    {
+      cerr << argv[i] << " ERR_LENGTH : RGB values must be either 3 or 6 characters long\n";
+    }
   }
   return 0;
 }
 
-bool all_hex(char* word) {
-  for(int i=0; i<strlen(word); i++) {
-    if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
-      || (word[i] >= 'A' && word[i] <='F')) {
+bool all_hex(char *word)
+{
+  for (int i = 0; i < strlen(word); i++)
+  {
+    if ((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f') || (word[i] >= 'A' && word[i] <= 'F'))
+    {
       continue;
     }
-    else {
+    else
+    {
+      cerr << word[i] << " ERR_VALUE: RGB values must be in 0-F range\n";
       return false;
     }
   }
 
-  //no non-hex characters found
+  // no non-hex characters found
   return true;
 }
