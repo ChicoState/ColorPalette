81d758d50014dd8f6d0f53eb506eca8a582e92fe
avankineni <avankineni@mail.csuchico.edu>
2024-02-21 11:19:14 -0800

    Fixed length and value validation for RGB values, closes issue #1

--- main.cpp
+++ main.cpp
@@ -6,14 +6,31 @@ using namespace std;
 bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
+  // Check if no values are provided
+  if(argc < 2) {
+    cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces" << endl;
+    return 0; // Exit the program after displaying the message
+  }
+
   int valid_colors = 0;
 
   for(int i=1; i < argc; i++) {
     int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
-    }  
+    if( !(length == 3 || length == 6) ) {
+      // Print error message for invalid length
+      cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+      continue; // Move to the next argument
+    }
+    if(!all_hex(argv[i])) {
+      // Print error message for invalid hexadecimal value
+      cout << argv[i] << " ERR_VALUE: RGB values must be in 0-F range" << endl;
+      continue; // Move to the next argument
+    }
+
+    // If the value passes both checks, print it
+    cout << "#" << argv[i] << endl;
   }
+
   return 0;
 }
 
@@ -28,6 +45,6 @@ bool all_hex(char* word) {
     }
   }
 
-  //no non-hex characters found
+  // No non-hex characters found
   return true;
 }
