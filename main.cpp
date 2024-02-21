14ede59a930285c452990a4e9ad8c8019beb8296
mcsharland <mcsharland@mail.csuchico.edu>
2024-02-21 11:40:03 -0800

    Fixes issue 2, Automate building with continuous integration workflow

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cstring>
 
-using namespace std;
-
 bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
@@ -11,7 +9,7 @@ int main(int argc, char* argv[]) {
   for(int i=1; i < argc; i++) {
     int length = strlen(argv[i]);
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
+      std::cout << "#" << argv[i] << std::endl;
     }  
   }
   return 0;
