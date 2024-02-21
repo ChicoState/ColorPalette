cabb0cea785158990b5fb81cdc0f09ea88505d9f
Mariah McFarlane <mjmcfarlane@csuchico.edu>
2024-02-21 11:17:09 -0800

    Improves Error Messaging

--- main.cpp
+++ main.cpp
@@ -12,8 +12,16 @@ int main(int argc, char* argv[]) {
     int length = strlen(argv[i]);
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
       cout << "#" << argv[i] << endl;
-    }  
+    } else {
+      cout << length << " ERR_LENGTH: RGB values must be either 3 or 6 characters long"
+    }
   }
+
+  if(argc < 2){
+    cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces"
+  }
+
+
   return 0;
 }
 
@@ -24,6 +32,7 @@ bool all_hex(char* word) {
       continue;
     }
     else {
+      cout << word[i] << " ERR_VALUE: RGB values must be in 0-F range"
       return false;
     }
   }
