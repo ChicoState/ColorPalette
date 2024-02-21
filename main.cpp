99f065d4f2dda6bb8145f45fd711a878b0e3b958
PranavGopalak <145394690+PranavGopalak@users.noreply.github.com>
2024-02-21 11:34:11 -0800

    Added error messages

--- main.cpp
+++ main.cpp
@@ -20,11 +20,14 @@ bool all_hex(char* color) {
     int length = strlen(color);
     // Check if the color code's length is either 3 or 6
     if(length != 3 && length != 6) {
+        cout << "Invalid color code: " << color << endl;
         return false; // Return false if the length is not 3 or 6
     }
     // Check each character to confirm it's a valid hexadecimal digit
     for(int i = 0; i < length; ++i) {
         if(!((color[i] >= '0' && color[i] <= '9') || (color[i] >= 'a' && color[i] <= 'f') || (color[i] >= 'A' && color[i] <= 'F'))) {
+            cout << "Invalid color code: " << color << endl;
+            cout << "Color code must be a valid hexadecimal number" << endl;
             return false;  // Return false if a non-hexadecimal digit is found
         }
     }
