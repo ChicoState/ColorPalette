d91b945093d3b0a86e50b62ec0551cc674daa528
Blake Hufford <huffordblake@gmail.com>
2024-02-21 11:37:58 -0800

    fixed namespace

--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <cstring>
 
-using namespace std;
-
 bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
   int valid_colors = 0;
 
   for(int i=1; i < argc; i++) {
-    int length = strlen(argv[i]);
+    int length = std::strlen(argv[i]);
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
+      std::cout << "#" << argv[i] << endl;
     }
     else if(length != 3 && length != 6){
-      cout << argv[i] << "ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+      std::cout << argv[i] << "ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
       exit(1);
     }
     else{
-      cout << argv[i] << "ERR_VALUE: RGB values must be in 0-F range" << endl;
+      std::cout << argv[i] << "ERR_VALUE: RGB values must be in 0-F range" << endl;
       exit(1);
     }
   }
   if(argc < 2){
-    cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces" << endl;
+    std::cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces" << endl;
   }
   return 0;
 }
